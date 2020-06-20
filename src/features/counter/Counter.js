07abcd5a469d7cd36d3a4b733bffd205093794c0
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-06-20 16:25:49 +0530

    redux toolkit counter logs are added for testing

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+  counterSlice,
+  selectValue,
+} from "./counterSlice";
+import styles from "./Counter.module.css";
 
 export function Counter() {
   const count = useSelector(selectCount);
+  console.log("count", count);
+  console.log("selectCount", selectCount);
+  console.log("counterSlice ss", counterSlice);
+  const { name } = counterSlice;
+  console.log("name ss", name);
+  console.log("selectValue ss", useSelector(selectValue));
+
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   return (
     <div>
@@ -38,7 +47,7 @@ export function Counter() {
           className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
+          onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
           className={styles.button}
